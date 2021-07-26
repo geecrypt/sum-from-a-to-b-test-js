f8bb88c6ffb5163f909fd9191ace56b3c33e4d20
geecrypt <gavina352@gmail.com>
2021-07-26 10:54:45 -0700

    Implement recursive solution

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+const sum = (fromN, toN) => {
+  const result = fromN;
+  if (fromN !== toN) {
+    return result + sum(fromN + 1, toN);
+  } else {
+    return result;
+  }
+};
 
-function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
-}
+console.log(sum(3, 7));
 
 module.exports = sum;
